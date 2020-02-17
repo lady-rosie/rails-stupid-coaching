376b42ad9934ba25db524bcadb64ef25b0c25e97
Rose Grondin <rose.grondin@gmail.com>
2020-02-17 11:20:39 -0500

    make the answer work

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,16 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.include? "?"
+      return @answer = "Silly question, get dressed and go to work!"
+    elsif @question == "I am going to work"
+      return @answer = "Great!"
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+    return @answer
+  end
 end
